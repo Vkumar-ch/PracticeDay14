67e5879d3b640a6d0eda844d7d2c1c82d2ef9024
Vkumar-ch <115803804+Vkumar-ch@users.noreply.github.com>
2023-01-06 19:07:03 +0530

    MainLinkedList

---
+++ MainLinkedList.java
@@ -0,0 +1,99 @@
+package PracticeDay14;
+
+import java.util.LinkedList;
+    public class MainLinkedList {
+    	
+        public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList();
+		list.add(70);
+		list.add(30);
+		list.add(56);
+//		list.display();
+		
+		
+		
+		
+		
+//		list.append(56);
+//		list.append(30);
+//		list.append(70);
+//		list.display();
+	//	
+//		list.append(56);
+//		list.append(30);
+//		list.insert(70);
+//		list.display();
+//		System.out.println();
+		
+		
+//		list.append(56);
+//		list.append(30);
+//		list.append(70);
+//		list.display();
+//		System.out.println();
+//		System.out.println(list.pop());
+//		list.display();
+		
+	//	
+//		list.append(56);
+//		list.append(30);
+//		list.append(70);
+//		list.display();
+//		System.out.println();
+//		System.out.println(list.popLast());
+//		list.display();
+		
+		
+		
+//		Uc 7
+//		list.append(56);
+//		list.append(30);
+//		list.append(70);
+//		list.display();
+//		System.out.println();
+//		if (list.search(30) != null) {
+//			System.out.println("The data is found");
+//		} else {
+//			System.out.println("The data  not found");
+//		}
+		
+//		Uc8
+//		list.append(56);
+//		list.append(30);
+//		list.append(70);
+//		list.display();
+//		System.out.println();
+//		if (list.search(30) != null) {
+//			System.out.println("The data is found");
+//		} else {
+//			System.out.println("The data  not found");
+//		}
+//		System.out.println(list.insertAfter(30, 40));
+//		list.display();
+	//	
+		
+		
+//		Uc9
+//		list.append(56);
+//		list.append(30);
+//		list.append(70);
+//		list.display();
+//		System.out.println();
+//		if (list.search(30) != null) {
+//			System.out.println("The data is found");
+//		} else {
+//			System.out.println("The data  not found");
+//		}
+//		System.out.println(list.insertAfter(30, 40));
+//		list.display();
+//		System.out.println();
+	//	
+//		list.popElement(30);
+//		list.display();
+//		System.out.println();
+//		System.out.println("The size is "+list.size());
+		
+		}
+	
+
+}
