e67596be7305d458ce82a92d731a20f9fd6c2930
Vkumar-ch <115803804+Vkumar-ch@users.noreply.github.com>
2023-01-06 19:09:08 +0530

    Queue

---
+++ Queue.java
@@ -0,0 +1,25 @@
+package PracticeDay14;
+
+	import java.util.LinkedList;
+
+
+	public class Queue {
+
+		LinkedList list = new LinkedList<>();
+
+		void push(Object key) {
+			list.add(key);
+		}
+
+		void print() {
+			list.size();
+		}
+
+		void pop(Object key) {
+			list.pop();
+
+		}
+
+	}
+
+
